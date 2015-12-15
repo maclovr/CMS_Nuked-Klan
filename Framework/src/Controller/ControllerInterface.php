255fc2ec6dc95d12432ec47a0abb2682298d0eef
ceednee <ceednee@ceednees-iMac.local>
2015-12-15 18:44:34 +0100

    Initial commit

---
+++ Framework/src/Controller/ControllerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Nuked-Klan package.
+ *
+ * (c) Nuked-Klan <xxx@nuked-klan.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NukedKlan\Framework\Controller;
+
+interface ControllerInterface
+{
+    public function render();
+}
---
+++ Framework/src/Http/Request.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Nuked-Klan package.
+ *
+ * (c) Nuked-Klan <xxx@nuked-klan.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NukedKlan/Framework/Http;
+
+use Symfony\Component\HttpFoundation\Request as BaseRequest;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class Request extends BaseRequest
+{
+
+    public static function createFromGlobals()
+    {
+        $request = parent::createFromGlobals();
+        $storage = new NativeSessionStorage();
+        $session = new Session($storage);
+
+        $request->setSession($session);
+
+        return $request;
+    } 
+} 
